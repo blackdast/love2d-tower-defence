899282177bd49f64337dd74b0dece1cd239554bb
blackdast <blackdastt@gmail.com>
2022-02-15 15:54:17 +0300

    Added pathfinding for enemy.

--- enemy.lua
+++ enemy.lua
@@ -1,9 +1,9 @@
 enemies = {}
 
 function spawnEnemy(x, y)
-    local enemy = world:newRectangleCollider(x, y, 32, 32,  { collision_class = "enemy" })
-    enemy.direction = 1
-    enemy.speed = 200
+    local enemy = world:newRectangleCollider(x + 32, y + 32, 32, 32,  { collision_class = "enemy" })
+    enemy.currentStep = 1
+    enemy.speed = 2
     enemy.animation = animations.enemy
     table.insert(enemies, enemy)
 end
@@ -13,8 +13,36 @@ function updateEnemies(dt)
         e.animation:update(dt)
         local ex, ey = e:getPosition()
 
-        local speed = e.speed * dt * e.direction
-        e:setX(ex + speed)
+        local normalX = ex - 16
+        local normalY = ey - 16
+
+        for node, count in enemyPath:nodes() do
+            if count == (e.currentStep + 1) then
+                local nextX = node:getX() * 32
+                local nextY = node:getY() * 32
+
+                if (normalX == nextX) and (normalY == nextY) then
+                    e.currentStep = e.currentStep + 1
+                    break
+                end
+
+                if not (normalX == nextX) then
+                    local direction = 1
+                    if normalX > nextX then
+                        direction = -1
+                    end
+                    e:setX(ex + (e.speed * direction))
+                end
+
+                if not (normalY == nextY) then
+                    local direction = 1
+                    if normalY > nextY then
+                        direction = -1
+                    end
+                    e:setY(ey + (e.speed * direction))
+                end
+            end
+        end
     end
 end
 
@@ -22,6 +50,6 @@ function drawEnemies()
     for i, e in ipairs(enemies) do
         local ex, ey = e:getPosition()
 
-        e.animation:draw(sprites.enemySheet, ex, ey, nil, e.direction, 1, 50, 50)
+        e.animation:draw(sprites.enemySheet, ex, ey, nil, e.direction, 1, 48, 48)
     end
 end
--- main.lua
+++ main.lua
@@ -2,8 +2,15 @@ function love.load()
     love.window.setMode(1920, 1080)
 
     anim8 = require "libs/anim8/anim8"
-
     sti = require "libs/Simple-Tiled-Implementation/sti"
+    grid = require ("libs/Jumper/jumper.grid")
+    pathfinder = require ("libs/Jumper/jumper.pathfinder")
+
+    enemyPath = {}
+    enemyStartX = 0
+    enemyStartY = 0
+    enemyEndX = 0
+    enemyEndY = 0
 
     sprites = {}
     sprites.enemySheet = love.graphics.newImage("sprites/enemy.png")
@@ -23,8 +30,8 @@ function love.load()
 
     loadMap("Map-1")
 
-    count = 0
-    updateDelay = 5
+    count = 30.9
+    updateDelay = 31
 end
 
 function love.update(dt)
@@ -34,7 +41,7 @@ function love.update(dt)
 
     count = count + dt
     if count > updateDelay then
-        spawnEnemy(flagX, flagY)
+        spawnEnemy(enemyStartX, enemyStartY)
         count = 0
     end
 end
@@ -56,9 +63,19 @@ function loadMap(mapName)
     gameMap = sti("maps/" .. mapName .. ".lua")
 
     for i, obj in pairs(gameMap.layers["start"].objects) do
-        flagX = obj.x
-        flagY = obj.y
+        enemyStartX = obj.x
+        enemyStartY = obj.y
     end
+
+    for i, obj in pairs(gameMap.layers["end"].objects) do
+        enemyEndX = obj.x
+        enemyEndY = obj.y
+    end
+
+    local walkable = 2
+
+    local enemyPathFinder = pathfinder(grid(prepareGrid(gameMap.layers["grass"].data, gameMap.layers["grass"].width, gameMap.layers["grass"].height)), 'JPS', walkable)
+    enemyPath = enemyPathFinder:getPath(enemyStartX / 32 + 1, enemyStartY / 32 + 1, enemyEndX / 32 + 1, enemyEndY / 32 + 1)
 end
 
 function destroyAll()
@@ -71,3 +88,28 @@ function destroyAll()
         i = i - 1
     end
 end
+
+function prepareGrid(grid, width, height)
+    local preparedGrid = {}
+    for i = 1, height do
+        local string = {}
+        for j = 1, width do
+            table.insert(string, grid[i][j]["gid"])
+        end
+        table.insert(preparedGrid, string)
+    end
+    return preparedGrid
+end
+
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
